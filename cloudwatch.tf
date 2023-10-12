c0ee75e4e29887319d991eb9a3ba8250cd3631cf
habuka036 <osamu@habuka.jp>
2023-10-12 21:31:11 +0900

    resource to data

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "cloudwatch_log_group_ecs" {
+data "aws_cloudwatch_log_group" "cloudwatch_log_group_ecs" {
   name              = "/aws/ecs/${var.project}-${var.env}/${var.suffix}"
 }
 
